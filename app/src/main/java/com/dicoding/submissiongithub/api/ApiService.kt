eb8c457b100511efc7d7143efcf7894446d09414
Ubean <ubeannn@gmail.com>
2021-11-29 19:56:36 +0700

    Remove Token

--- app/src/main/java/com/dicoding/submissiongithub/api/ApiService.kt
+++ app/src/main/java/com/dicoding/submissiongithub/api/ApiService.kt
@@ -7,29 +7,24 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("/users")
-    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getUsers(): Call<List<UsersResponse>>
 
     @GET("/users/{username}")
-    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getDetailUser(
         @Path("username") username: String
     ): Call<DetailUserResponse>
 
     @GET("/users/{username}/followers")
-    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getFollowersUser(
         @Path("username") username: String
     ): Call<List<UsersResponse>>
 
     @GET("/users/{username}/following")
-    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getFollowingUser(
         @Path("username") username: String
     ): Call<List<UsersResponse>>
 
     @GET("/search/users")
-    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun searchUser(
         @Query("q") username: String
     ): Call<SearchResponse>
